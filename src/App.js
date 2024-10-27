d99a4a0b5714712608dd8b1ed18f97e0dd0005fb
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-10-27 13:42:25 -0400

    Created Wordle Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import Wordle from './components/Wordle';
 
 function App() {
   const [solution, setSolution] = useState(null);
@@ -13,7 +14,7 @@ function App() {
   return (
     <div className="App">
       <h1>Wordler</h1>
-      {solution && <div>Solution is: {solution}</div> }
+      {solution && <Wordle solution={solution} /> }
     </div>
   );
 }
