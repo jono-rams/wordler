0b618af1c4746dfbd95475049cff7888f6135f3d
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-10-27 12:52:06 -0400

    Get random word

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [solution, setSolution] = useState(null);
+
+  useEffect(() => {
+    fetch("https://random-word-api.herokuapp.com/word?length=5")
+     .then(response => response.json())
+     .then(data => setSolution(data[0]))
+  }, [setSolution]);
+
   return (
     <div className="App">
       <h1>Wordler</h1>
+      {solution && <div>Solution is: {solution}</div> }
     </div>
   );
 }
