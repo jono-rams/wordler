1d12a5ff0de5032c00a072e217420897109a57a5
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-10-27 14:36:47 -0400

    Show current guess

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,4 +1,4 @@
-export default function Row({ guess }) {
+export default function Row({ guess, currentGuess }) {
   
   if (guess) {
     return (
@@ -6,6 +6,19 @@ export default function Row({ guess }) {
         {guess.map((letter, index) => (
           <div key={index} className={letter.color}>{letter.key}</div>
         ))}
+      </div>
+    )
+  }
+
+  if (currentGuess) {
+    let letters = currentGuess.split('');
+
+    return (
+      <div className="row">
+        {letters.map((letter, index) => (
+          <div key={index + letter} className='filled'>{letter}</div>
+        ))}
+        {[...Array(5- letters.length)].map((_, i) => (<div key={i}></div>))}
       </div>
     )
   }
