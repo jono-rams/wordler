1830f6d2ad914e1d69f10b88d704014a0f7695c0
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-10-27 15:35:06 -0400

    Ending a game

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import Wordle from './components/Wordle';
+import ReactConfetti from 'react-confetti';
 
 function App() {
   const [solution, setSolution] = useState(null);
+  const [winState, setWinState] = useState(null);
 
   useEffect(() => {
     fetch("https://random-word-api.herokuapp.com/word?length=5")
@@ -13,8 +15,9 @@ function App() {
 
   return (
     <div className="App">
+      {winState && <ReactConfetti width={window.innerWidth} height={window.innerHeight} tweenDuration={1000} /> }
       <h1>Wordler</h1>
-      {solution && <Wordle solution={solution} /> }
+      {solution && <Wordle solution={solution} setWinState={setWinState} /> }
     </div>
   );
 }
