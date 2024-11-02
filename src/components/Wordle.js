b16b0a0eec1c4025e8289ecb13f0979027210e8e
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-11-02 13:38:19 -0400

    revert to 1.1 (#5)

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,21 +1,13 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import useWordle from "../hooks/useWordle";
 import Grid from "./Grid";
 import Keypad from "./Keypad";
 import Modal from "./Modal";
 
-export default function Wordle({ setShowConfetti }) {
-  const { currentGuess, handleKeyUp, guesses, isCorrect, turn, usedKeys, solution, newGame, uid, setUid, getSolution } = useWordle();
+export default function Wordle({ solution, setShowConfetti }) {
+  const { currentGuess, handleKeyUp, guesses, isCorrect, turn, usedKeys } = useWordle(solution);
   const [showModal, setShowModal] = useState(false);
   const [confettiShown, setConfettiShown] = useState(false);
-  const startNew = useRef(newGame);
-
-  useEffect(() => {
-    if (uid) { return; }
-
-    startNew.current()
-      .then(uid => setUid(uid));
-  }, [uid, setUid]);
 
   useEffect(() => {
     window.addEventListener('keyup', handleKeyUp);
@@ -31,22 +23,18 @@ export default function Wordle({ setShowConfetti }) {
       window.removeEventListener('keyup', handleKeyUp);
     }
     if (turn > 5) {
-      if (!solution) {
-        getSolution().then(() => {
-          setTimeout(() => setShowModal(true), 2000)
-          window.removeEventListener('keyup', handleKeyUp);
-    })
-  }
-  }
+      setTimeout(() => setShowModal(true), 2000)
+      window.removeEventListener('keyup', handleKeyUp);
+    }
 
     return () => window.removeEventListener('keyup', handleKeyUp);
-  }, [handleKeyUp, isCorrect, setShowConfetti, turn, confettiShown, getSolution, solution]);
+  }, [handleKeyUp, isCorrect, setShowConfetti, turn, confettiShown]);
 
   return (
     <div>
       <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
       <Keypad handleKeyUp={handleKeyUp} usedKeys={usedKeys} isCorrect={isCorrect} />
-      {solution && showModal && <Modal isCorrect={isCorrect} turn={turn} solution={solution} />}
+      {showModal && <Modal isCorrect={isCorrect} turn={turn} solution={solution} />}
     </div>
   )
 }
