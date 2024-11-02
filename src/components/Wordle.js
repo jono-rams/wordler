b7b62760052749fe22bebbc798769046b13d1d4a
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-11-02 13:17:07 -0400

    v2.0 (#3)
    * Added Fallback API
    
    * v2.0
    
    * Added Node.js server to handle getting word and testing guess
    
    * Added dependencies to useEffect

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,13 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import useWordle from "../hooks/useWordle";
 import Grid from "./Grid";
 import Keypad from "./Keypad";
 import Modal from "./Modal";
 
-export default function Wordle({ solution, setShowConfetti }) {
-  const { currentGuess, handleKeyUp, guesses, isCorrect, turn, usedKeys } = useWordle(solution);
+export default function Wordle({ setShowConfetti }) {
+  const { currentGuess, handleKeyUp, guesses, isCorrect, turn, usedKeys, solution, newGame, uid, setUid, getSolution } = useWordle();
   const [showModal, setShowModal] = useState(false);
   const [confettiShown, setConfettiShown] = useState(false);
+  const startNew = useRef(newGame);
+
+  useEffect(() => {
+    if (uid) { return; }
+
+    startNew.current()
+      .then(uid => setUid(uid));
+  }, [uid, setUid]);
 
   useEffect(() => {
     window.addEventListener('keyup', handleKeyUp);
@@ -23,18 +31,22 @@ export default function Wordle({ solution, setShowConfetti }) {
       window.removeEventListener('keyup', handleKeyUp);
     }
     if (turn > 5) {
-      setTimeout(() => setShowModal(true), 2000)
-      window.removeEventListener('keyup', handleKeyUp);
-    }
+      if (!solution) {
+        getSolution().then(() => {
+          setTimeout(() => setShowModal(true), 2000)
+          window.removeEventListener('keyup', handleKeyUp);
+    })
+  }
+  }
 
     return () => window.removeEventListener('keyup', handleKeyUp);
-  }, [handleKeyUp, isCorrect, setShowConfetti, turn, confettiShown]);
+  }, [handleKeyUp, isCorrect, setShowConfetti, turn, confettiShown, getSolution, solution]);
 
   return (
     <div>
       <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
       <Keypad handleKeyUp={handleKeyUp} usedKeys={usedKeys} isCorrect={isCorrect} />
-      {showModal && <Modal isCorrect={isCorrect} turn={turn} solution={solution} />}
+      {solution && showModal && <Modal isCorrect={isCorrect} turn={turn} solution={solution} />}
     </div>
   )
 }
